0d3e48c57a0b84c2d22f5ae6517fbce2b09ed072
EliasAspoeck <elias.aspoeck@gmail.com>
2023-03-10 11:16:24 +0100

    impruv usabilytü

--- Encryptor/Program.cs
+++ Encryptor/Program.cs
@@ -2,28 +2,41 @@
 
 var enc = new EncryptionService();
 
-Console.WriteLine("Enter directory to encrypt/decrypt");
-var dir = Console.ReadLine();
+var password = "orderMeALiverhut";
+
 while (true)
 {
-    Console.WriteLine("Enter 'e' to encrypt or 'd' to decrypt");
+    Console.Write("Enter directory to encrypt/decrypt: ");
+    var dir = Console.ReadLine();
+    Console.Write("Enter 'e' to encrypt or 'd' to decrypt: ");
     var input = Console.ReadLine();
     if (input == "e")
     {
+        GetPassword();
         Console.WriteLine("Encrypting file...");
         var files = Directory.GetFiles(dir,"*.*", SearchOption.AllDirectories).ToList();
-        files.ForEach(f => enc.FileEncrypt(f, "test"));
+        // files.ForEach(f => enc.FileEncrypt(f, password));
+        Console.WriteLine("Files Encrypted");
     }
 
     if (input == "d")
     {
+        GetPassword();
         Console.WriteLine("Decrypting file...");
         var files = Directory.GetFiles(dir,"*.*", SearchOption.AllDirectories).ToList();
-        files.ForEach(f => enc.FileDecrypt(f, "test"));
+        // files.ForEach(f => enc.FileDecrypt(f, password));
+        Console.WriteLine("Files Decrypted");
     }
     
     enc.DeleteFiles();
 }
 
-
+void GetPassword() {
+    Console.Write("Enter Password: ");
+    var pw = Console.ReadLine();
+    if (!string.IsNullOrEmpty(pw))
+    {
+        password = pw;
+    }
+}
 
